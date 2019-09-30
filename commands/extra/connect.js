c16eacbcfff353342c4594ff26e15381fc8037c2
Brian <bpluhar@fuselenses.com>
2019-09-30 11:16:39 -0400

    Test

---
+++ commands/extra/connect.js
@@ -0,0 +1,24 @@
+const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
+const { Pool, Client } = require('pg');
+
+
+module.exports = class connect extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'connect',
+            group: 'extra',
+            memberName: 'connect',
+            description: 'Creates a connect pool.',
+            userPermissions: ['ADMINISTRATOR'],
+        });
+    }
+    run(message) {
+
+      let x = process.env.User;
+
+      message.reply(x);
+
+    });
+    }
+};
