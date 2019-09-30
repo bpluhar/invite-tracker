91f0636b004e339f35b4fffd13c650eaebdb9415
Brian <bpluhar@fuselenses.com>
2019-09-30 11:27:00 -0400

    Connection test

--- commands/extra/connect.js
+++ commands/extra/connect.js
@@ -2,6 +2,13 @@ const { Command } = require('discord.js-commando');
 const { MessageEmbed } = require('discord.js');
 const { Pool, Client } = require('pg');
 
+const pool = new Pool({
+  user: 'nimrgldfvtcpxs',
+  host: 'ec2-174-129-226-232.compute-1.amazonaws.com',
+  database: 'dvf45uru2evs7',
+  password: 'dab5cf11240c436785e142b91e7a1cf46c4819444a1279c819538061cc2ed7ab',
+  port: 5432,
+})
 
 module.exports = class connect extends Command {
     constructor(client) {
@@ -15,9 +22,10 @@ module.exports = class connect extends Command {
     }
     run(message) {
 
-      let x = process.env.User;
-
-      message.reply(x);
+      pool.query('SELECT NOW()', (err, res) => {
+        message.reply(`${res}`);
+        pool.end
+      })
 
     }
 };
