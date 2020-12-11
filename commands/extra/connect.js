104a4fd2f404a2556d59654fb2d9c96722c8a721
Brian Pluhar <25670731+bpluhar@users.noreply.github.com>
2020-12-11 11:21:06 -0500

    Update connect.js

--- commands/extra/connect.js
+++ commands/extra/connect.js
@@ -3,11 +3,11 @@ const { MessageEmbed } = require('discord.js');
 const { Pool, Client } = require('pg');
 
 const pool = new Pool({
-  user: 'nimrgldfvtcpxs',
-  host: 'ec2-174-129-226-232.compute-1.amazonaws.com',
-  database: 'dvf45uru2evs7',
-  password: 'dab5cf11240c436785e142b91e7a1cf46c4819444a1279c819538061cc2ed7ab',
-  port: 5432,
+  user: '',
+  host: '',
+  database: '',
+  password: '',
+  port: ,
 })
 
 module.exports = class connect extends Command {
